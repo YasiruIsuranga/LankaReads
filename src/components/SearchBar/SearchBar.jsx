3878795f981f64ed25046b5441fe0742e6e55667
Yasiru Isuranga <yasiruisuranga1@gmail.com>
2024-08-14 16:05:11 +0530

    error handling

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './SearchBar.css';
+import { Link } from 'react-router-dom';
 
 const SearchBar = () => {
   return (
     <div className="input-group mb-3 search-bar">
       <div className="input-group-prepend">
-        <button  className="btn btn-danger dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Category</button>
-        <div className="dropdown-menu">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <a className="dropdown-item" href="#">Something else here</a>
-          <div role="separator" className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Separated link</a>
-        </div>
+        <button  className="btn btn-danger dropdown-toggle" type="button" data-toggle="dropdown" id="dropdown" aria-haspopup="true" aria-expanded="false">Category</button>
+        <ul className="dropdown-menu" aria-labelledby="dropdown">
+          <li><a className="dropdown-item" href="#">Action</a></li>
+          <li><a className="dropdown-item" href="#">Another action</a></li>
+          <li><a className="dropdown-item" href="#">Something else here</a></li>
+          <li><div role="separator" className="dropdown-divider"></div></li>
+          <li><a className="dropdown-item" href="#">Separated link</a></li>
+        </ul>
       </div>
       <input type="text" style={{borderRadius:'10px', marginLeft:'10px'}} className="form-control search-input" placeholder="Search Books Here" aria-label="Search Books Here" aria-describedby="button-addon2"/>
       <div className="input-group-append">
